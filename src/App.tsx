bc61d45fcabef78452f7ef755a3c795b2287291c
dani <118696363+dwleung@users.noreply.github.com>
2024-02-01 12:02:13 -0500

    Add initial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import IdeaPage from "./pages/GenerateIdeaPage/GenerateIdeaPage";
+import IdeaDetailsPage from "./pages/IdeaDetailsPage/IdeaDetailsPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/idea" element={<IdeaPage />} />
+				<Route path="/idea/details" element={<IdeaDetailsPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
