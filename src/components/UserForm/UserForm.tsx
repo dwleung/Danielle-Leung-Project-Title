772e7be98414a35017925d221899a0e2b391e31f
dani <118696363+dwleung@users.noreply.github.com>
2024-02-04 23:10:29 -0500

    Add form validation for empty fields

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -8,86 +8,191 @@ interface FormProps {
 	isSignUpForm: boolean;
 }
 
+interface FormValues {
+	[key: string]: string | undefined;
+}
+
 export default function UserForm(props: FormProps) {
 	const navigate = useNavigate();
-	// 	const [values, setValues] = useState({})
-	// 	const [errors, setErrors] = useState<string[]>([]);
-	// 	const [touched,setTouched] = useState(false);
+	const [values, setValues] = useState<FormValues>({});
+	const [errors, setErrors] = useState<FormValues>({});
+	const [touched, setTouched] = useState<FormValues>({});
+	const [focused, setFocused] = useState<string | null>(null);
 
-	// 	//change event handler
-	// 	const handleChange = (e) =>{
-	// 		const {name, value: newValue,type} = e.target;
-	// 		setValues({
-	// 			...values,
-	// 			[name]: value,
-	// 		})
-	// 	}
+	//Function to handle input changes
+	const handleChange = (e: any) => {
+		const { name, value } = e.target;
 
-	// 	//check passwords equal each other
-	// 	//disable submit button if there are form errors
-	// const nameValidation = (fieldName, fieldValue) =>{
-	// 	if (fieldValue.trim()=== ""){
-	// 		return `${fieldName} is required`;
-	// 	}
-	// 	if (fieldValue.trim().length < 3) {
-	// 		return `${fieldName} needs to be at least 3 characters`
-	// 	}
-	// }
-	// const passwordCheck = (password, repeatPassword) => {
-	// 	if (repeatPassword !== password) {
-	// 		return `Passwords do not match`
-	// 	}
-	// }
+		setValues({
+			...values,
+			[name]: value,
+		});
 
-	// const validate ={
-	// 	name: name => nameValidation('name', name);
+		if (value.trim() === "") {
+			setErrors({ ...errors, [name]: "This field is required" });
+		} else {
+			setErrors({ ...errors, [name]: "" });
+		}
+	};
 
-	// }
+	//Function to handle when an input is focused
+	const handleFocus = (fieldName: string) => {
+		setFocused(fieldName);
+	};
+
+	//Function to handle when an input is touched (onBlur event)
+	const handleBlur = (e: any) => {
+		const { name, value } = e.target;
+		setFocused(null); //Clear the focused input when it loses focus
+		setTouched({ ...touched, [name]: true });
+		if (!value.trim()) {
+			setErrors({ ...errors, [name]: "This field is required" });
+		}
+	};
+
+	const inputClassName = (fieldName: string) => {
+		if (focused === fieldName) {
+			return "input--focus";
+		} else if (errors[fieldName]) {
+			return "input--error";
+		}
+		return;
+	};
+
+	//check passwords equal each other
+	//disable submit button if there are form errors
+	const passwordCheck = (fieldName: string, matchingField: string) => {
+		if (matchingField !== fieldName) {
+			setErrors({
+				...errors,
+				[fieldName]: "Passwords do not match",
+				[matchingField]: "Passwords do not match",
+			});
+		}
+	};
 
 	return (
 		<div className="form-main">
 			<form className="form" onSubmit={props.onSubmitFn}>
 				{props.isSignUpForm && (
 					<>
-						<label htmlFor="name">NAME</label>
+						<div className="form__label-wrapper">
+							<label
+								className="form__label"
+								htmlFor="name"
+							>
+								NAME
+							</label>
+							{touched.name && errors.name && (
+								<p className="error-message">
+									{errors["name"]}
+								</p>
+							)}
+						</div>
 						<input
-							className="input form__input"
+							className={`input form__input ${inputClassName(
+								"name"
+							)}`}
 							type="text"
 							name="name"
+							value={values.name}
+							onBlur={handleBlur}
+							onChange={handleChange}
+							onFocus={() => handleFocus("name")}
 							placeholder="Enter a name"
+							required
 						/>
 					</>
 				)}
-				<label htmlFor="username">USERNAME</label>
-				<input
-					className="input form__input"
-					type="text"
-					name="username"
-					placeholder="Enter a username"
-				/>
-
-				<label htmlFor="password">PASSWORD</label>
-				<input
-					className="input form__input"
-					type="password"
-					name="password"
-					placeholder="Enter a password"
-				/>
+				<>
+					<div className="form__label-wrapper">
+						<label className="form__label" htmlFor="username">
+							USERNAME
+						</label>
+						{touched.username && errors.username && (
+							<p className="error-message">
+								{errors["username"]}
+							</p>
+						)}
+					</div>
+					<input
+						className={`input form__input ${inputClassName(
+							"username"
+						)}`}
+						type="text"
+						name="username"
+						value={values.username}
+						onBlur={handleBlur}
+						onChange={handleChange}
+						onFocus={() => handleFocus("username")}
+						placeholder="Enter a username"
+						required
+					/>
+				</>
+				<>
+					{" "}
+					<div className="form__label-wrapper">
+						<label className="form__label" htmlFor="password">
+							PASSWORD
+						</label>
+						{touched.password && errors.password && (
+							<p className="error-message">
+								{errors["password"]}
+							</p>
+						)}
+					</div>
+					<input
+						className={`input form__input ${inputClassName(
+							"password"
+						)}`}
+						type="password"
+						name="password"
+						value={values.password}
+						onBlur={handleBlur}
+						onChange={handleChange}
+						onFocus={() => handleFocus("password")}
+						placeholder="Enter a password"
+						required
+					/>
+				</>
 				{props.isSignUpForm && (
 					<>
-						<label htmlFor="passwordrepeat">
-							RE-ENTER PASSWORD
-						</label>
+						<div className="form__label-wrapper">
+							<label
+								className="form__label"
+								htmlFor="passwordrepeat"
+							>
+								RE-ENTER PASSWORD
+							</label>
+							{touched.password && errors.password && (
+								<p className="error-message">
+									{errors["passwordRepeat"]}
+								</p>
+							)}
+						</div>
 						<input
-							className="input form__input"
+							className={`input form__input ${inputClassName(
+								"passwordRepeat"
+							)}`}
 							type="password"
-							name="passwordrepeat"
+							name="passwordRepeat"
+							value={values.passwordRepeat}
+							onBlur={handleBlur}
+							onChange={handleChange}
+							onFocus={() => handleFocus("passwordRepeat")}
 							placeholder="Re-enter password"
+							required
 						/>
 					</>
 				)}
 				<div className="form__button-wrapper">
-					<button type="submit" className="button">
+					<button
+						type="submit"
+						className="button"
+						onClick={() => {
+							passwordCheck("password", "passwordRepeat");
+						}}
+					>
 						{props.buttonText}
 					</button>
 					<button
